8f1b43eaaaa893066794df57f7445cf6c9571598
刘阳 <249606097@qq.com>
2020-09-01 11:25:11 +0800

    style: 修改返回参数形式

--- apps/account/views.py
+++ apps/account/views.py
@@ -11,39 +11,29 @@ def register(request):
     if not request.method == 'POST':
         return process_response({'code': '002', 'msg': '请求方法错误'})
     else:
-        content = {}
-
         # 数据经过中间件处理存放在 request.json_data 中
         json_data = request.json_data
 
         # 验证码 captcha 检验
         captcha = json_data['captcha']
         if request.session.get('captcha', '').lower() != captcha.lower():
-            content['code'] = '004'
-            content['msg'] = '验证码错误'
-            return process_response(content)
+            return process_response({'code': '004', 'msg': '验证码错误'})
 
-        # 用户名 username 检验
+        # 用户名 username 格式检验
         username = json_data['username']
         result = validate_username(username)
         if result:
-            content['code'] = result[0]
-            content['msg'] = result[1]
-            return process_response(content)
+            return process_response({'code': result[0], 'msg': result[1]})
 
-        # 密码 password 验证
+        # 密码 password 格式验证
         password = json_data['password']
         result = validate_password(password)
         if result:
-            content['code'] = result[0]
-            content['msg'] = result[1]
-            return process_response(content)
+            return process_response({'code': result[0], 'msg': result[1]})
 
         # 用户名 username 存在性验证
         if account_models.User.objects.filter(username=username):
-            content['code'] = '121'
-            content['msg'] = '用户名已存在'
-            return process_response(content)
+            return process_response({'code': '121', 'msg': '用户名已存在'})
 
         # 创建用户 user 和 用户信息 user_info
         user = account_models.User(username=username,
@@ -53,7 +43,4 @@ def register(request):
         user_info = account_models.UserInfo(user=user)
         user_info.save()
 
-        content['code'] = '000'
-        content['msg'] = '注册成功'
-
-        return process_response(content)
+        return process_response({'code': '000', 'msg': '注册成功'})
